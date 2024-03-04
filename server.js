42cd2d8508900deb730db2b977b7bc6900045450
jiyamittal25 <jiyamittal25@gmail.com>
2024-03-04 13:00:25 +0530

    changes in server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express=require("express");
 const app=express();
 const dbConfig=require('./db');
+const roomRoute=require('./routes/roomRoute');
+
+app.use("/api/rooms",roomRoute);
+
 const port =process.env.PORT || 5000;
-app.listen(port,()=>console.log("server running on port"));
+app.listen(port, () => console.log(`Server running on port ${port}`)); 
+
+
